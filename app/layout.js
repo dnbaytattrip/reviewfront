a40639ca7966692132b4dc6a5e1b7d2595004a0a
rana <rana.buddy@gmail.com>
2025-07-02 22:44:39 +0600

    feat:first commit

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Document",
-  description: "Document",
+  title: "Review",
+  description: "Review",
 };
 
 export default function RootLayout({ children }) {
